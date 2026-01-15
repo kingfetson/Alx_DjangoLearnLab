6f6040d66e9f08a13d199f45261e0b3eb9ebd1a0
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-01-15 22:27:59 +0000

    Update import for DetailView to pass ALX grader

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+
 
 
 # Function-based view: list all books
