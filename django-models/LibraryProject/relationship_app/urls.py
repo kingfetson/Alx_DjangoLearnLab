f216ba956ad83caeabf520fdfdda8a49a49cb7e1
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-01-15 22:47:13 +0000

    Add user authentication views, templates, and URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
 
+from .views import register_view, login_view, logout_view
+
 app_name = 'relationship_app'
 
 urlpatterns = [
@@ -10,4 +12,9 @@ urlpatterns = [
     
     # Class-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication URLs
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -16,3 +20,40 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+# -------------------------------
+# User Registration
+# -------------------------------
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in user after registration
+            return redirect('relationship_app:list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# -------------------------------
+# User Login
+# -------------------------------
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('relationship_app:list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# -------------------------------
+# User Logout
+# -------------------------------
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
