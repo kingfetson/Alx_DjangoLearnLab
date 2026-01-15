18a409667a63ffeb5ce31c88f1eb0ca2d23d2faf
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-01-15 22:36:34 +0000

    Fix urls.py imports to pass ALX grader

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . import views
 from .views import list_books
+from .views import LibraryDetailView
+
 app_name = 'relationship_app'
 
 urlpatterns = [
     # Function-based view
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
     
     # Class-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
