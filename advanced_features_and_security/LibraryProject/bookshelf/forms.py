667dacb05046a1596ff7820b6df29d065080ddf6
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-01-23 07:10:20 +0000

    Add ExampleForm to satisfy ALX form validation requirement

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Book
 
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    author = forms.CharField(max_length=255)
+    publication_year = forms.IntegerField()
+
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'publication_year']
+        fields = ["title", "author", "publication_year"]
