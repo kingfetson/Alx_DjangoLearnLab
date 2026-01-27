7151133177d4d78a6d23eda25f34d84e527ba87e
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-01-27 11:53:03 +0000

    Configure API URLs and include api.urls in project routing

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-
-# api.urls
+from django.urls import path
+from .views import BookList
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
