0b0b86f35872fbb0a7bafee61f9fd3f0fe7ffa94
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-02-05 09:55:40 +0000

    Enable filtering, search, and ordering with django-filter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 # Read-only operations - public read, authenticated write
