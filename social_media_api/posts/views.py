ec6f56ca96643c0c4f6dad08d51973073b9f0e6a
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-02-16 18:13:11 +0000

    Add posts and comments app with models, serializers, viewsets, and routing

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,45 @@
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission: Only the author can edit/delete.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions only to author
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
