228b3da77423070ff12465ea5f3f1502e2740ffe
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-02-20 11:31:56 +0000

    Configure database with environment variables including PORT

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="unsafe-secret-key-for-dev")
 
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG =False
 
 ALLOWED_HOSTS = [
     "localhost",
